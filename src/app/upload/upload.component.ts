583312a58fba2291ceff0a35e04c1c00c1a0909d
alex2wong <huangyixiu188@hotmail.com>
2017-06-06 20:57:52 +0800

    upload to port 8000/upload service

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -6,8 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
+  // this Component only upload File(using form method). got response in __blank
   compressImg: boolean = true;
   fileloaded: any;
+  form: any;
+  inputEle: any;
+  actionURL: string;
   constructor() { }
 
   ngOnInit() {
@@ -15,23 +19,38 @@ export class UploadComponent implements OnInit {
       name: "",
       type: "",
       size: "",
-    }
-    
+    }    
+    this.actionURL = "http://127.0.0.1:8000/upload";  
   }
 
   // upload img file..
   upload(evt) {
     console.warn("file Changed..");
     let $ele = evt.target;
-    if (($ele.files.length) > 0) {
+    if (($ele.files.length) > 0 && (/image\/\w+/.test($ele.files[0].type))) {
       this.fileloaded = $ele.files[0];
+      let reader = new FileReader();
+      reader.onload = function(result: ProgressEvent) {
+        let inputEle: HTMLInputElement = <HTMLInputElement>document.querySelector('#file2upload');
+        inputEle.style.backgroundImage = "url(" + reader.result + ")";
+      }
+      reader.readAsDataURL(this.fileloaded);
+    } else {
+      alert("请选择图片文件");
+      this.fileloaded = {
+        name: "",
+        type: "",
+        size: "",
+      }
     }
   }
 
   // submit the img to server..
   submit(evt) {
     // server path: http://111.231.11.20:3000/upload/
-    console.warn("uploading to http://111.231.11.20:3000/upload/ , compressed:" +　this.compressImg);
-    // http.PUT()
+    console.warn("uploading to " + this.actionURL + " , compressed:" +　this.compressImg);
+    this.form = document.querySelector('#imageForm'); 
+    
+    this.form.submit();
   }
 }
